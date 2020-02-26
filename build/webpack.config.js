38b4a855ba61456a00f012e0cb50bee40b811dc0
depeng <kongdepeng520@gmail.com>
2020-02-26 19:06:14 +0800

    build: resolve js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
         test:/\.css$/,
         use:['vue-style-loader', 'style-loader','css-loader']
       },
